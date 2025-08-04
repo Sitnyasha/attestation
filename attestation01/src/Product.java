50a480ef043e8871d2fd75e82016296b436abf61
Sitnyasha <igorsit96@gmail.com>
2025-08-04 20:46:52 +0300

    создание продуктов

---
+++ attestation01/src/Product.java
@@ -0,0 +1,51 @@
+import java.util.Objects;
+
+public class Product {
+    private String name;
+    private int price;
+
+    public Product(String name, int price) {
+        setName(name);
+        setPrice(price);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        if (name == null || name.isEmpty()) {
+            throw new IllegalArgumentException("Имя не может быть пустым");
+        }
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Деньги не могут быть отрицательными");
+        }
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return price == product.price && name.equals(product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, price);
+    }
+}
